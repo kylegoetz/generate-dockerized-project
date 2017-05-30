a36059ad3518e7154ef78e1a3363e798dcb36bbe
Kyle Goetz <kylegoetz@gmail.com>
2017-05-29 22:24:09 -0500

    Dev environment spins up faster

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # generate-dockerized-project
 for rapidly generating an ExpressJS API and React UI with an nginx reverse proxy
 
-1. Clone repo
-2. run `./create.sh <APP_NAME>`
+* `mkdir <APP> && cd <APP>`
+* `git clone <this repo>`
+* `./create.sh`
 
-Dockers will all be set up and started. A run script will be generated as well for later launching.
+This generates the UI, API, and reverse proxy docker images and the source code for them in the current directory (`<APP>`).
+
+To run the docker images, run `run.sh` from `<APP>`. Then you can open `http://localhost:8080` in your browser to view the app.
+Live reload exists for the UI. Work in progress for API.
--- create.sh
+++ create.sh
@@ -2,15 +2,12 @@
 
 # First arg is the name of the app (hyphenated version)
 
-docker run -v `pwd`:/app -e APP=$1 kylegoetz/create-react-app
-docker run -v `pwd`:/app -e APP=$1 kylegoetz/express-generator:latest
+docker run -v `pwd`:/app kylegoetz/create-react-app:latest
+docker run -v `pwd`:/app kylegoetz/express-generator:latest
 
 # run the docker compose script that creates an nginx, ui, and api dockers linked together
 # docker-compose up
 
-echo "docker run -d -it --expose 3000 --name application-ui -w /app -v `pwd`/$1-ui:/app node:7-alpine npm start ; docker run -d -it --expose 3000 --name application-api -w /app -v `pwd`/$1-api:/app node:7-alpine npm start ; docker run -d -p 8080:80 -it -v `pwd`/nginx.conf:/etc/nginx/nginx.conf --link application-ui --link application-api nginx" > run.sh
-chmod +x run.sh
-
-docker run -d -it --expose 3000 --name application-ui -w /app -v `pwd`/$1-ui:/app node:7-alpine npm start
-docker run -d -it --expose 3000 --name application-api -w /app -v `pwd`/$1-api:/app node:7-alpine npm start
-docker run -d -p 8080:80 -it -v `pwd`/nginx.conf:/etc/nginx/nginx.conf --link application-ui --link application-api nginx
+#docker run -d -it --expose 3000 --name application-ui -w /app -v `pwd`/$1-ui:/app node:7-alpine npm start
+#docker run -d -it --expose 3000 --name application-api -w /app -v `pwd`/$1-api:/app node:7-alpine npm start
+#docker run -d -p 8080:80 -it -v `pwd`/nginx.conf:/etc/nginx/nginx.conf --link application-ui --link application-api nginx
