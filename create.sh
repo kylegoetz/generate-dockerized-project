093cada6a05c471ca445c63fa7bcd234697c3353
Kyle Goetz <kylegoetz@gmail.com>
2017-05-30 16:27:22 -0500

    added verbosity to project spin-up

--- create.sh
+++ create.sh
@@ -2,12 +2,8 @@
 
 # First arg is the name of the app (hyphenated version)
 
+echo "Creating the application framework (this will take a few minutes)"
+echo "Beginning by creating the React frontend..."
 docker run -v `pwd`:/app kylegoetz/create-react-app:latest
+echo "And now creating the Express API backend"
 docker run -v `pwd`:/app kylegoetz/express-generator:latest
-
-# run the docker compose script that creates an nginx, ui, and api dockers linked together
-# docker-compose up
-
-#docker run -d -it --expose 3000 --name application-ui -w /app -v `pwd`/$1-ui:/app node:7-alpine npm start
-#docker run -d -it --expose 3000 --name application-api -w /app -v `pwd`/$1-api:/app node:7-alpine npm start
-#docker run -d -p 8080:80 -it -v `pwd`/nginx.conf:/etc/nginx/nginx.conf --link application-ui --link application-api nginx
