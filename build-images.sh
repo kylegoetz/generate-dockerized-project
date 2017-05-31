b4e29558224c80f3cdce10fe9a2fa5752d6781ab
Kyle Goetz <kylegoetz@gmail.com>
2017-05-31 14:08:14 -0500

    changed build script

--- build-images.sh
+++ build-images.sh
@@ -1,10 +1,10 @@
 # Invoke this build script with ./build.sh <USERNAME>
 
 # Build UI generator
-build -t $1/create-react-app ./create-react-app-docker
+build -t kylegoetz/create-react-app ./create-react-app-docker
 
 # Build API generator
-build -t $1/express-generator ./express-generator-docker
+build -t kylegoetz/express-generator ./express-generator-docker
 
-# Build nodemon container
-build -t $1/nodemon ./nodemon-docker
+# Build Development container for API work (needed because of mysql and sequelize-cli dependencies)
+build -t kylegoetz/development ./development
