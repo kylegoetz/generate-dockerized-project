93968a01109e93c4548bc65145c25eac7f097d6d
Kyle Goetz <kylegoetz@gmail.com>
2017-05-31 13:49:35 -0500

    added verbosity to project creation sript

--- create.sh
+++ create.sh
@@ -4,6 +4,7 @@
 
 echo "Creating the application framework (this will take a few minutes)"
 echo "Beginning by creating the React frontend..."
-docker run -v `pwd`:/app kylegoetz/create-react-app:latest
+docker run -v `pwd`:/app kylegoetz/create-react-app
 echo "And now creating the Express API backend"
-docker run -v `pwd`:/app kylegoetz/express-generator:latest
+docker run -v `pwd`:/app kylegoetz/express-generator
+echo "Done! Make sure to use the included install.sh to install npm packages rather than npm directly in the terminal"
